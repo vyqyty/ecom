73c7967ad2dbcc452d7554abfef87b014ac3bd78
vyqyty <vyqyty@gmail.com>
2020-11-19 10:08:48 +0000

    part16

--- EcomApp/views.py
+++ EcomApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from EcomApp.models import Setting
 
 # Create your views here.
 def Home(request):
-    context = {}
+    setting = Setting.objects.get(id=1)
+    context = {
+        'setting': setting,
+    }
     return render(request, 'home.html', context)
