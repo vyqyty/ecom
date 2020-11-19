6cbeb5fc3dfada002dfacc2a43b8728f310c9328
vyqyty <vyqyty@gmail.com>
2020-11-19 08:49:31 +0000

    part13

--- Product/admin.py
+++ Product/admin.py
@@ -8,7 +8,7 @@ admin.site.register(Category)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status', 'created_at', 'updated_at']
+    list_display = ['title', 'status', 'created_at', 'updated_at', 'image_tag']
     list_filter = ['title', 'created_at']
     list_per_page = 10
     search_fields = ['title', 'new_price', 'detail']
--- Product/models.py
+++ Product/models.py
@@ -53,3 +53,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" heights="70" width="60" />'.format(self.image.url))
+    image_tag.short_description = 'Image'
