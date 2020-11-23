5810e058eef118dc82ecf77f69514816490de34d
vyqyty <vyqyty@gmail.com>
2020-11-23 03:45:05 +0000

    Part20

--- EcomApp/urls.py
+++ EcomApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, product_single
 
 
 urlpatterns = [
     path('', Home, name='home'),
+    path('product/<int:id>/', product_single, name='product_single'),
 ]
--- EcomApp/views.py
+++ EcomApp/views.py
@@ -4,11 +4,24 @@ from EcomApp.models import Setting
 from Product.models import Product
 
 # Create your views here.
+
+
 def Home(request):
     setting = Setting.objects.get(id=1)
     sliding_images = Product.objects.all().order_by('id')[:2]
+    latest_products = Product.objects.all().order_by('-id')
+    products = Product.objects.all()
+
     context = {
         'setting': setting,
         'sliding_images': sliding_images,
+        'latest_products': latest_products,
+        'products': products,
     }
     return render(request, 'home.html', context)
+
+
+def product_single(request, id):
+    context = {}
+
+    return render(request, 'product-single.html', context)
